14218998fc2859e50da8c3b497d1f9c039bea6b0
Catisinha <luacoutinhor@gmail.com>
2023-10-09 09:40:06 -0300

    Iniciada a criação do login 2

--- MtecDevs/Controllers/AccountController.cs
+++ MtecDevs/Controllers/AccountController.cs
@@ -1,32 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
-namespace MtecDevs.Controllers
+namespace MtecDevs.Controllers;
+
+public class AccountController : Controller
 {
-    [Route("[controller]")]
-    public class AccountController : Controller
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
     {
-        private readonly ILogger<AccountController> _logger;
+        _logger = logger;
+    }
 
-        public AccountController(ILogger<AccountController> logger)
-        {
-            _logger = logger;
-        }
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
     }
 }
